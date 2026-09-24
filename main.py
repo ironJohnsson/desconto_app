7d5593aa3cd1f3ac7b913c74ffc0049fb61f8a6c
ironJohnsson <matheusluiz.johnsson@gmail.com>
2026-09-24 10:03:21 -0300

    Arquitetura em camadas: implementação da camada de serviços.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from src.models.desconto import DescontoVIP
+from src.models.desconto import DescontoNormal, DescontoVIP, DescontoPremium
 from src.models.pedido import Pedido
 from src.services.pedido_service import PedidoService
 
 if __name__ == "__main__":
-    pedido = Pedido("Leonardo", DescontoVIP())
-
-    valor_final = pedido.valor_final(100)
 
     service = PedidoService()
-    service.adicionar_pedido(pedido)
+
+    """Criando pedidos e aplicando descontos"""
+
+    pedido1 = Pedido("Alice", DescontoNormal())
+    pedido1.valor_original = 100.0 #definindo valor original do pedido 1
+
+    pedido2 = Pedido("Bob", DescontoVIP())
+    pedido2.valor_original = 200.0 #definindo valor original do pedido 2
+
+    pedido3 = Pedido("Eva", DescontoPremium())
+    pedido3.valor_original = 300.0 #definindo valor original do pedido 3
+
+    #Criando instâncias de pedidos e adicionando ao serviço para aplicação dos descontos
+
+    service.adicionar_pedido(pedido1)
+    service.adicionar_pedido(pedido2)
+    service.adicionar_pedido(pedido3)
+
+
     service.processar_pedidos()
